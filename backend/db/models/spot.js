3323aa597febab6c1571f0f102425478729d7097
joemcdonough15 <joemcdonoughmusic@gmail.com>
2024-09-21 20:44:42 -0400

    Create SpotImages Table and setup one to many association with Spots table

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -2,14 +2,15 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Spot.belongsTo(models.User, {
         foreignKey: "ownerId",
+      });
+
+      Spot.hasMany(models.SpotImage, {
+        foreignKey: "spotId",
+        onDelete: "CASCADE",
+        hooks: true,
       });
     }
   }
