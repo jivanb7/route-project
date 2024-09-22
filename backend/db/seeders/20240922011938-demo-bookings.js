fe3805f74727f0eb7a43a4cb9d642c9d19112ad0
joemcdonough15 <joemcdonoughmusic@gmail.com>
2024-09-21 22:32:17 -0400

    Generate seed and seed Bookings table

--- backend/db/seeders/20240922011938-demo-bookings.js
+++ backend/db/seeders/20240922011938-demo-bookings.js
@@ -4,22 +4,32 @@ const { Booking } = require("../models");
 
 const bookingsData = [
   {
+    userId: 1,
+    spotId: 3,
     startDate: "2025-11-19",
     endDate: "2025-11-20",
   },
   {
+    userId: 2,
+    spotId: 5,
     startDate: "2024-10-19",
     endDate: "2024-10-25",
   },
   {
+    userId: 1,
+    spotId: 5,
     startDate: "2026-01-19",
     endDate: "2026-01-30",
   },
   {
+    userId: 3,
+    spotId: 1,
     startDate: "2024-12-15",
     endDate: "2024-12-20",
   },
   {
+    userId: 2,
+    spotId: 2,
     startDate: "2025-04-12",
     endDate: "2025-04-15",
   },
