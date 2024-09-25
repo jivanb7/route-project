84b2f7ccd06b120e192714ef6b3416908990fc68
joemcdonough15 <joemcdonoughmusic@gmail.com>
2024-09-24 22:53:54 -0400

    Write route to add review image to review by reviewId

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -8,6 +8,30 @@ const { Review, Spot, ReviewImage, SpotImage } = require("../../db/models");
 
 const router = express.Router();
 
+const reviewNotFoundError = new Error(
+  "Couldn't find a Review with the specified id"
+);
+reviewNotFoundError.status = 404;
+reviewNotFoundError.message = "Review couldn't be found";
+
+const reviewAuthorization = async (req, _res, next) => {
+  const userId = req.user.id;
+
+  const { reviewId } = req.params;
+
+  const review = await Review.findByPk(reviewId);
+
+  if (!review) {
+    return next(reviewNotFoundError);
+  }
+
+  if (userId !== review.userId) {
+    return blockAuthorization(next);
+  }
+
+  next();
+};
+
 // get all reviews written by the current user
 router.get("/current", requireAuth, async (req, res, next) => {
   const userId = req.user.id;
@@ -54,5 +78,43 @@ router.get("/current", requireAuth, async (req, res, next) => {
 
   res.status(200).json({ Reviews: reviewsArray });
 });
+
+// create new image for a review specified by reviewId
+
+router.post(
+  "/:reviewId/images",
+  requireAuth,
+  reviewAuthorization,
+  async (req, res, next) => {
+    const { reviewId } = req.params;
+    const review = await Review.findOne({
+      where: {
+        id: reviewId,
+      },
+      include: [{ model: ReviewImage }],
+    });
+
+    if (review.ReviewImages.length === 10) {
+      const err = new Error(
+        "Cannot add any more images because there is a maximum of 10 images per resource"
+      );
+      err.status = 403;
+      err.message = "Maximum number of images for this resource was reached";
+      return next(err);
+    }
+
+    let newReviewImage = await ReviewImage.create({
+      ...req.body,
+      reviewId,
+    });
+
+    newReviewImage = newReviewImage.toJSON();
+
+    res.status(201).json({
+      id: newReviewImage.id,
+      url: newReviewImage.url,
+    });
+  }
+);
 
 module.exports = router;
